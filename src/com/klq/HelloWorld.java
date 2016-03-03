04040416e5fcf0d26d1a225dc056e28461f63edf
klq1419426 <1419426440@qq.com>
2016-03-03 16:15:06 +0800

    Signed-off-by: klq1419426 <1419426440@qq.com>222

--- src/com/klq/HelloWorld.java
+++ src/com/klq/HelloWorld.java
@@ -8,6 +8,7 @@ public class HelloWorld {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("11111111111111");
+		System.out.println("22222222222222");
 	}
 
 }
